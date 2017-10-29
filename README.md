47e4e6a42e4baf84b45e1901b7b4225495e4a3f9
xdass <420600@gmail.com>
2017-10-29 12:35:12 +0500

    Init commit

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+Script scans the entered directory and find duplicate files and output path to them.
 
+# Script examples
+```bash
+$ python duplicates.py C:\Users\Dass\PycharmProjects\11_duplicates
+C:\Users\Dass\PycharmProjects\11_duplicates\.git\logs\HEAD
+C:\Users\Dass\PycharmProjects\11_duplicates\.git\logs\refs\remotes\origin\HEAD
+C:\Users\Dass\PycharmProjects\11_duplicates\testDir\one\common\file1.txt
+C:\Users\Dass\PycharmProjects\11_duplicates\testDir\two\files\file1.txt
+C:\Users\Dass\PycharmProjects\11_duplicates\testDir\one\common\shlak.r
+C:\Users\Dass\PycharmProjects\11_duplicates\testDir\two\files\shlak.r
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,48 @@
+import os
+import sys
+import hashlib
+
+
+def get_file_md5(path):
+    hasher = hashlib.md5()
+    filename = os.path.basename(path).encode()
+    block_size = 128 * hasher.block_size
+    hasher.update(filename)
+    try:
+        with open(path, 'rb') as file:
+            for chunk in iter(lambda: file.read(block_size), b''):
+                hasher.update(chunk)
+            file_hash = hasher.hexdigest()
+        return file_hash
+    except FileNotFoundError:
+        return None
+
+
+def get_duplicate_files(file_hashes):
+    dup_files = list(file_path for path_list in file_hashes.values() if len(path_list) > 1 for file_path in path_list)
+    return dup_files
+
+
+def crawling_directory(path):
+    processed_files = {}
+    for dir_path, dir_names, file_names in os.walk(path):
+        if file_names:
+            for file_name in file_names:
+                full_path = os.path.join(dir_path, file_name)
+                file_md5 = get_file_md5(full_path)
+                if file_md5 in processed_files.keys():
+                    processed_files[file_md5].append(full_path)
+                else:
+                    processed_files[file_md5] = [full_path]
+    return processed_files
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        directory_path = sys.argv[1]
+        crawl_results = crawling_directory(directory_path)
+        duplicates_files = get_duplicate_files(crawl_results)
+
+        for file in duplicates_files:
+            print(file)
+    else:
+        print('Укажите путь к папке: python duplicates.py <directory_path>')
